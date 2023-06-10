97f7d9a1432d1f90e0d11a28d822f0399aafe3b7
Setsuna Yukiii <siang-meng@live.com>
2023-06-11 00:51:36 +0800

    adding animation for landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "../pictures/AMAGPT copy.png";
 // import leftCharacter from "../pictures/Hayate_Stamp.png";
 // import rightCharacter from "../pictures/kokoasama.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import leftCharacter from "../pictures/leftchar.png";
 import rightCharacter from "../pictures/rightchar.png";
 import { Typography, Box, Link } from "@mui/material";
@@ -32,6 +34,9 @@ function LandingPage() {
         var access = document.getElementById(keyFeatures[0].id);
         access.scrollIntoView({ behavior: "smooth" }, true);
     };
+    useEffect(() => {
+        AOS.init({ duration: 1000 });
+    }, []);
     return (
         <Box>
             <Box
@@ -44,25 +49,23 @@ function LandingPage() {
             >
                 {" "}
                 <img src={logo} alt="App Logo" style={{ margin: "auto" }} />
-                <Typography variant="h5" sx={{ margin: "30px" }}>
+                <Typography
+                    variant="h5"
+                    sx={{
+                        margin: "30px",
+                        "&:hover": {
+                            scale: "1.5",
+                            cursor: "pointer",
+                        },
+                        transition: "scale 0.5s",
+                    }}
+                >
                     AMA (Ask me anything)! Literally ANYTHING!
                 </Typography>
                 <Box>
                     <img src={leftCharacter} alt="Left Character" />
                     <img src={rightCharacter} alt="Right Character" />
                 </Box>
-                {/* <Link
-                    onClick={() => {
-                        goDown();
-                    }}
-                    sx={{
-                        color: "black",
-                        "&:hover": {
-                            color: "#08C5AE",
-                            cursor: "pointer",
-                        },
-                    }}
-                > */}
                 <ArrowDownwardIcon
                     onClick={() => {
                         goDown();
@@ -74,7 +77,9 @@ function LandingPage() {
                         "&:hover": {
                             color: "#08C5AE",
                             cursor: "pointer",
+                            scale: "1.4",
                         },
+                        transition: "scale 0.5s",
                         animation: "bounce 1s infinite",
                         "@keyframes bounce": {
                             "0%": { transform: "translateY(0)" },
@@ -83,30 +88,22 @@ function LandingPage() {
                         },
                     }}
                 />
-                {/* </Link> */}
-                {/* <Typography
-                    variant="h4"
-                    sx={{
-                        color: "#05BDA7",
-                        fontFamily: "Inika",
-                        fontWeight: "bold",
-                    }}
-                >
-                    Try us for free today!
-                </Typography> */}
             </Box>
             {keyFeatures.map((item, index) => {
                 const { title, description, redirect, id, image } = item;
                 const titleDisplay = index + 1 + ". " + title;
                 return (
-                    <KeyFeature
-                        key={index}
-                        title={titleDisplay}
-                        description={description}
-                        redirect={redirect}
-                        id={id}
-                        image={image}
-                    />
+                    <div data-aos="fade-down" key={index}>
+                        <KeyFeature
+                            data-aos="fade-down"
+                            key={index}
+                            title={titleDisplay}
+                            description={description}
+                            redirect={redirect}
+                            id={id}
+                            image={image}
+                        />
+                    </div>
                 );
             })}
         </Box>
