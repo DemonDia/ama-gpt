070bd6da591c0b57d55fd8570d8562ee3a6b8ac5
Setsuna Yukiii <siang-meng@live.com>
2023-06-10 19:11:02 +0800

    style: improvement of chat side menu

--- src/components/Chats/ChatSide.js
+++ src/components/Chats/ChatSide.js
@@ -6,67 +6,74 @@ function ChatSide({ chats, selectChat, selectedChat }) {
         <Box
             sx={{
                 marginTop: "10px",
-                overflowY: "scroll",
+
                 height: "60vh",
             }}
         >
             <Typography variant={"h5"}>Chats</Typography>
-            {chats && chats.length > 0 ? (
-                <>
-                    {chats.map((chat) => {
-                        const { chatName, id } = chat;
-                        return (
-                            <Button
-                                key={id}
-                                onClick={() => {
-                                    selectChat({ chatName, id });
-                                }}
-                                variant={"contained"}
-                                sx={{
-                                    height: "30px",
-                                    width: "90%",
-                                    textAlign: "left",
-                                    justifyContent: "start",
-                                    overflow: "hidden",
-                                    textOverflow: "ellipsis",
-                                    whiteSpace: "nowrap",
-                                    background:
-                                        selectedChat && selectedChat.id == id
-                                            ? "#08C5AE"
-                                            : "white",
-                                    color:
-                                        selectedChat && selectedChat.id == id
-                                            ? "white"
-                                            : "black",
-                                    // boxShadow:"none",
-                                    margin: "5px",
-                                    "&:hover": {
+            <Box sx={{ overflowY: "scroll", height: "100%" }}>
+                {chats && chats.length > 0 ? (
+                    <>
+                        {chats.map((chat) => {
+                            const { chatName, id } = chat;
+                            return (
+                                <Box
+                                    key={id}
+                                    onClick={() => {
+                                        selectChat({ chatName, id });
+                                    }}
+                                    variant={"contained"}
+                                    sx={{
+                                        borderRadius: "5px",
+                                        width: "90%",
+                                        textAlign: "left",
+                                        justifyContent: "start",
+                                        overflow: "hidden",
+                                        textOverflow: "ellipsis",
+                                        whiteSpace: "nowrap",
                                         background:
                                             selectedChat &&
                                             selectedChat.id == id
-                                                ? "rgba(8, 197, 174, 0.7)"
-                                                : "rgba(250, 250, 250, 1)",
-                                    },
-                                }}
-                            >
-                                {chatName}
-                            </Button>
-                        );
-                    })}
-                </>
-            ) : (
-                <>
-                    <Box
-                        sx={{
-                            padding: "10px",
-                            background: "rgb(150 150 150 / 10%)",
-                            boxRadius: "5px",
-                        }}
-                    >
-                        <Typography variant={"h6"}>No chats yet</Typography>
-                    </Box>
-                </>
-            )}
+                                                ? "#08C5AE"
+                                                : "#EBEBEB",
+                                        color:
+                                            selectedChat &&
+                                            selectedChat.id == id
+                                                ? "white"
+                                                : "black",
+                                        margin: "10px auto",
+                                        padding: "10px",
+                                        "&:hover": {
+                                            background:
+                                                selectedChat &&
+                                                selectedChat.id == id
+                                                    ? "rgba(8, 197, 174, 0.7)"
+                                                    : "#08C5AE",
+                                            color: "white",
+                                            cursor: "pointer",
+                                        },
+                                        transition: "background 0.5s"
+                                    }}
+                                >
+                                    {chatName}
+                                </Box>
+                            );
+                        })}
+                    </>
+                ) : (
+                    <>
+                        <Box
+                            sx={{
+                                padding: "10px",
+                                background: "rgb(150 150 150 / 10%)",
+                                boxRadius: "5px",
+                            }}
+                        >
+                            <Typography variant={"h6"}>No chats yet</Typography>
+                        </Box>
+                    </>
+                )}
+            </Box>
         </Box>
     );
 }
